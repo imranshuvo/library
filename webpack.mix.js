db2ab6a93874c9502a784adf39c3e6cf0b3bf6f4
imranshuvo <imrankhanshuvo@gmail.com>
2022-05-19 21:42:39 +0600

    Improved the front layout more

--- webpack.mix.js
+++ webpack.mix.js
@@ -15,7 +15,3 @@ mix.js('resources/js/app.js', 'public/js')
     .postCss('resources/css/app.css', 'public/css', [
         require('tailwindcss'),
     ]);
-
-
-    mix.browserSync('127.0.0.1:8001');
-
