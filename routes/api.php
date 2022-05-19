ee5984e95e944e9210ef78eca8ecdcf59219ed8e
imranshuvo <imrankhanshuvo@gmail.com>
2022-05-19 23:28:47 +0600

    Added API token authentication

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\AuthController;
 use App\Http\Controllers\BookController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
@@ -15,16 +16,19 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/books', [BookController::class, 'index']);
-Route::get('/books/{id}',[BookController::class, 'show']);
-Route::get('/books/search/{name}', [BookController::class, 'search']);
 
 
-Route::post('/books', [BookController::class, 'store']);
-Route::post('/books/{id}',[BookController::class, 'update']);
-Route::post('/books/delete/{id}', [BookController::class, 'destroy']);
+Route::prefix('books')->group( function() {
+    Route::get('/', [BookController::class, 'index']);
+    Route::get('/{id}',[BookController::class, 'show']);
+    Route::get('/search/{name}', [BookController::class, 'search']);
+});
 
+Route::post('/admin/register', [AuthController::class, 'register']);
+Route::post('/admin/login', [AuthController::class, 'login']);
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+Route::group(['middleware' => ['auth:sanctum']], function(){
+    Route::post('/books', [BookController::class, 'store']);
+    Route::put('/books/{id}',[BookController::class, 'update']);
+    Route::delete('/books/{id}', [BookController::class, 'destroy']);
 });
